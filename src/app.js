2cdf06b960b4d049b13d375922eee71c99089e65
LineJinJ <102582136+LineJinJ@users.noreply.github.com>
2022-07-01 12:24:40 +0200

    Injected the weather forecast showing one forecast. How can we inject several weather forecasts?

--- src/app.js
+++ src/app.js
@@ -21,6 +21,29 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = `
+  <div class="row">
+  <div class="col-2">
+ <div class="weather-forecast-date">
+   Thu
+  </div>
+<img src="http://openweathermap.org/img/wn/50d@2x.png" alt=""
+    width="42">
+  <div class="weather-forecast-temperature">
+<span class="weather-forecast-temperature-max">
+     18°
+     </span>
+    <span class="weather-forecast-temperature-min">
+     12°
+    </span>
+      </div>
+    </div> 
+    </div>
+`;
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -82,3 +105,5 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("New York");
+
+displayForecast();
